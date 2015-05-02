883fb00b257c0728f94fb09dd7f18876e27d06f9
Trevor Prinn <trev@babbacom.com>
2015-05-02 15:10:40 +0100

    Prepared for release.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Babbacombe.Logger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Babbacombe Computers Logger dll")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Babbacombe Computers Ltd")]
 [assembly: AssemblyProduct("Babbacombe.Logger")]
 [assembly: AssemblyCopyright("Copyright © Babbacombe Computers Ltd 2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
